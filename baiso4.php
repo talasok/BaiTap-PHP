1a8b97e4f5b7d86719d03fc489f3f740a0c683c9
hoang <hohoang511@gmail.com>
2019-10-07 22:12:44 +0700

    add edit delete

---
+++ baiso4.php
@@ -0,0 +1,204 @@
+<?php include_once ("header.php")?>
+<?php include_once ("nav.php")?>
+<?php 
+    #Code bài số 4
+    include_once("model/book.php");
+    // $book = new Book(50,"OOP in PHP","ndungit",2019);
+   
+    $keyWord = null;
+    if(isset($_REQUEST["search"])){
+        $keyWord =$_REQUEST["search"];
+    }
+    if (isset($_REQUEST["addBook"])) {
+        $id = $_REQUEST["id"];
+        $title = $_REQUEST["title"];
+        $price = $_REQUEST["price"];
+        $author = $_REQUEST["author"];
+        $year = $_REQUEST["year"];
+        $content = $id . "#" . $title . "#" . $price . "#" . $author . "#" . $year;
+        Book::addToFile($content);
+        
+    }
+    if (isset($_REQUEST["editBook"])) {
+        $id    = $_REQUEST["id"];
+        $title = $_REQUEST["title"];
+        $price = $_REQUEST["price"];
+        $author= $_REQUEST["author"];
+        $year  = $_REQUEST["year"];
+        $book = new Book($id,$price,$title,$author,$year);
+        Book::editItem($book,Book::getListFromFile());
+    }
+    if(isset($_REQUEST["action"])){
+        if(strcmp($_REQUEST["action"],"xoa")==0){
+            Book::delete(Book::getListFromFile(),$_REQUEST["id"]);
+        }
+    } 
+    
+    //$ls=Book::getList(); //Mockdata
+    $sl=Book::getListFromFile();
+    $lsFromFile=Book::getSearch($keyWord);
+    ?>
+<div class="container" >
+    <div>
+        <form action="" method="get" style="display: flex;">
+            <input type="text" name="search" value="<?php echo $keyWord;?>" class="form-control" placeholder="Search for..." >
+            <div class="input-group-append">
+                <button class="btn btn-primary" type="submit">
+                    <i class="fas fa-search"></i>
+                </button>
+            </div>
+        </form>
+    </div>
+    <!-- thêm -->
+    <div class="row" style="padding-top: 40px;">
+        <button data-toggle="modal" data-target="#addModal" class="btn btn-outline-primary" style="margin-left: 1025px"><i class="fas fa-plus-circle"></i> Thêm</button>
+    </div>
+    <!-- modal -->
+    <form action="baiso4.php" method="post">
+        <div class="modal fade" id="addModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Thêm</h5>
+                    <button type="submit" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                        <fieldset>
+                            <div class="form-group d-flex">
+                                <label class="pt-1 col-md-2 control-label" for="Title">ID</label>
+                                <div class="col-md-10">
+                                    <input id="id" name="id" type="text" placeholder="ID" class="form-control input-md">
+                                </div>
+                            </div>
+                            <div class="form-group d-flex">
+                                <label class="pt-1 col-md-2 control-label" for="Title">Title</label>
+                                <div class="col-md-10">
+                                    <input id="title" name="title" type="text" placeholder="Title" class="form-control input-md">
+                                </div>
+                            </div>
+                            <div class="form-group d-flex">
+                                <label class="pt-1 col-md-2 control-label" for="Title">Price</label>
+                                <div class="col-md-10">
+                                    <input id="price" name="price" type="text" placeholder="Price" class="form-control input-md">
+                                </div>
+                            </div>
+                            <!-- Select Basic -->
+                            <div class="form-group d-flex">
+                                <label class="pt-1 col-md-2 control-label" for="Year">Year</label>
+                                <div class="col-md-10">
+                                    <input type="text" id="year" name="year" class="form-control input-md">   
+                                </div>
+                            </div>
+
+                            <!-- Text input-->
+                            <div class="form-group d-flex">
+                                <label class="pt-1 col-md-2 control-label" for="Author">Author</label>
+                                <div class="col-md-10">
+                                    <input id="author" name="author" type="text" placeholder="Author" class="form-control input-md">
+
+                                </div>
+                            </div>
+                        </fieldset>
+                    </div>  
+                <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="submit" name="addBook" class="btn btn-primary">Save changes</button>
+            </div>
+            </div>
+        </div>
+        </div>
+    </form>
+    <!-- modal sửa -->
+    <form action="baiso4.php" method="get">
+        <div class="modal fade" id="editModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Sửa</h5>
+                        <button type="submit" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                            <fieldset>
+                                <div class="form-group d-flex">
+                                    <label class="pt-1 col-md-2 control-label" for="Title">ID</label>
+                                    <div class="col-md-10">
+                                        <input  id="id" name="id" type="text" placeholder="ID" class="form-control input-md" value="">
+                                    </div>
+                                </div>
+                                <div class="form-group d-flex">
+                                    <label class="pt-1 col-md-2 control-label" for="Title">Title</label>
+                                    <div class="col-md-10">
+                                        <input id="title" name="title" type="text" value="" placeholder="Title" class="form-control input-md">
+                                    </div>
+                                </div>
+                                <div class="form-group d-flex">
+                                    <label class="pt-1 col-md-2 control-label" for="Title">Price</label>
+                                    <div class="col-md-10">
+                                        <input id="price" name="price" type="text" value="" placeholder="Price" class="form-control input-md">
+                                    </div>
+                                </div>
+                                <!-- Select Basic -->
+                                <div class="form-group d-flex">
+                                    <label class="pt-1 col-md-2 control-label" for="Year">Year</label>
+                                    <div class="col-md-10">
+                                        <input type="text" id="year" name="year" value="" class="form-control input-md"> 
+                                    </div>
+                                </div>
+
+                                <!-- Text input-->
+                                <div class="form-group d-flex">
+                                    <label class="pt-1 col-md-2 control-label" for="Author">Author</label>
+                                    <div class="col-md-10">
+                                        <input id="author" name="author" value="" type="text" placeholder="Author" class="form-control input-md">
+                                    </div>
+                                </div>
+                            </fieldset>
+                        </div>  
+                    <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="submit" name="editBook" class="btn btn-primary">Save changes</button>
+                </div>
+                </div>
+            </div>
+        </div>
+    </form>
+    <!-- table -->
+    <div class="row">
+        <table class ="table table-bordered" >
+            <thead class="thead-dark" align="center">
+                <tr>
+                    <th colspan="1"> STT</th>
+                    <th colspan="2"> Title</th>
+                    <th colspan="2"> Pricec</th>
+                    <th colspan="2"> Author</th>
+                    <th colspan="2">Year</th>
+                    <th colspan="6"> Thao tác </th  >
+                  </tr>
+             </thead>
+            <tbody align="center">
+               <?php 
+               //độ dài list ls
+               // count($ls)
+               foreach ($lsFromFile as $key => $value) {
+                ?>
+               <tr>
+                    <th colspan="1"><?php echo $key ?></th>
+                    <td colspan="2"><?php echo $value->title ?></td>
+                    <td colspan="2"><?php echo $value->price?></td>
+                    <td colspan="2"><?php echo $value->author ?></td>
+                    <td colspan="2"> <?php echo $value->year ?></td> 
+                    <td> <button data-toggle="modal" data-target="#editModal" class="btn btn-outline-warning" name="BtnEdit"><i class="fas fa-edit"></i>Sửa </button>
+                    <a href="baiso4.php?action=xoa&&id=<?php echo $key ?>" type="submit" class="btn btn-outline-danger" nanme="deleteBook"><i class="fas fa-trash-alt"></i>Xóa </a>
+                    </td>
+               </tr>
+               <?php }?>
+            </tbody>
+        </table>
+    </div>
+</div>
+
+<?php include_once ("footer.php")?>
---
+++ model/book.php
@@ -0,0 +1,109 @@
+<?php
+class Book
+{
+    #Begin properties
+    var $id;
+    var $title;
+    var $price;
+    var $author;
+    var $year;
+    #end properties
+
+    #Construct function
+    function __construct($id, $title ,$price, $author, $year)
+    {
+        $this->id = $id;
+        $this->title = $title;
+        $this->price = $price;
+        $this->author = $author;
+        $this->year = $year;
+    }
+
+    #Member function
+    function display()
+    {
+        echo "id: " . $this->id. "<br>";
+        echo "Title: " . $this->title . "<br>";
+        echo "Price: " . $this->price . "<br>";
+        echo "Author: " . $this->author . "<br>";
+        echo "Year: " . $this->year . "<br>";
+    }
+
+    static function getList(){
+        $listBook = array();
+        array_push($listBook,new Book(1,"OOP in PHP",3,"ndungithue",2019));
+    }
+    /**
+     * lấy dữ liệu từ file
+     */
+    static function getListFromFile(){
+        $arrData = file("data/book.txt");
+        $lsBook = array();
+        // var_dump($arrData);
+        foreach ($arrData as $key => $value) {
+            $arrItem =explode("#",$value);
+            $book = new Book($arrItem[0],$arrItem[1],$arrItem[2],$arrItem[3],$arrItem[4]);
+            array_push($lsBook,$book);
+        }
+        return $lsBook;
+    }
+    static function getSearch($search = null){
+        $arrData = file("data/book.txt");
+        $lsBook = [];
+        // var_dump($arrData);
+        foreach ($arrData as $key => $value) {
+            $arrItem =explode("#",$value);
+            if(
+                $search ==null ||
+                strlen(strstr($arrItem[0],$search)) ||
+                strlen(strstr($arrItem[1],$search)) ||
+                strlen(strstr($arrItem[2],$search)) ||
+                strlen(strstr($arrItem[3],$search)) ||
+                strlen(strstr($arrItem[4],$search)) 
+            )
+            {
+                $book = new Book($arrItem[0],$arrItem[1],$arrItem[2],$arrItem[3],$arrItem[4]);
+                array_push($lsBook,$book);
+            }
+        }
+        return $lsBook;
+    }
+    static function getSearchID($id){
+        $arrData = file("data/book.txt");
+        foreach ($arrData as $key => $value) {
+            if(strlen(strstr($arrItem[0],$id)){
+                return new Book($arrItem[0],$arrItem[1],$arrItem[2],$arrItem[3],$arrItem[4]);
+            }
+        }
+        return null;
+    }
+    static function addToFile($content)
+    {
+        $myfile = fopen("data/book.txt", "a") or die("Unable to open file!");
+        fwrite($myfile, "\n".$content);
+        fclose($myfile);
+    }
+    static function editItem($newItem,$arrdata){
+        $myfile = fopen("data/book.txt", "w") or die("Unable to open file!");
+        foreach ($arrdata as $key => $value) {
+            if($value->id === $newItem->id){
+                $value->title = $newItem->title;
+                $value->price = $newItem->price;
+                $value->author = $newItem->author;
+                $value->year = $newItem->year;    
+            }
+            $content = $value->id."#".$value->title."#".$value->price."#".$value->author."#".$value->year;
+            fwrite($myfile, $content);
+        }
+        fclose($myfile);
+    }
+    static function delete($arrdata,$id){
+        $myfile = fopen("data/book.txt", "w") or die("Unable to open file!");
+        unset($arrdata[$id]);
+        foreach ($arrdata as $key => $value) {
+            $content = $value->id."#".$value->title."#".$value->price."#".$value->author."#".$value->year;
+            fwrite($myfile, $content);
+        }
+        fclose($myfile);
+    }
+}
